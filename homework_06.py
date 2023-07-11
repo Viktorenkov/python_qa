5d1da0155c5457d837e6f001a6e799bcee8ffa98
Viktorenkov <128530621+Viktorenkov@users.noreply.github.com>
2023-07-11 12:16:08 +0300

    Update homework_06.py

--- homework_06.py
+++ homework_06.py
@@ -1,33 +1,48 @@
-#task_1_створити АРІ на базі гугл таблиці, містить поля "назва товару", "опис товару", "ціна" (інтова чи флоат), 
-#"залишок" (інтовий чи флоат), "містить глютен" (булеве тру чи фолс (виставляєте прапорцем). заповнити мінімум 10 позицій. 
-#дані мають бути отримати по зовнішньому ключу "goods" (not "data")
+"""
+Convert a non-negative integer num to its English words representation.
+Example 1:
+Input: num = 123
+Output: "One Hundred Twenty Three"
 
-#table_of_goods
-#https://docs.google.com/spreadsheets/d/10IwMC1NOmxflfPwlzdv7cxV07mMXbUTzu2sxtl6LL2k/edit?usp=share_link
-#json url
-#https://script.googleusercontent.com/macros/echo?user_content_key=BnNVjCpPhZ4YNeWWbDknC3HxvgKOmAAo8h1N1gtEi3s9EGpCCFERzrffD8mYM4PsvcBbVdBPKAOfmMWlsJYSUlVYx7JyIuIQm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnK5zQBDYXl_mOYuXyF-VGZwsRZC82cQjklNZEu9_4Qs7QvIZ4BxQ_UowCgNBYqkFcdSKJN26chW0Yt3M7F61M_1iTFGgMiFE3tz9Jw9Md8uu&lib=MoPXG4wonAChj-B1OziooWc2_kmfSSr-t
-
-#task_2_за допомогою requests завантажити створені дані. порахувати вартість всіх товарів та товарів без глютена.
-
-from pprint import pprint
-import requests
-
-url = 'https://script.googleusercontent.com/macros/echo?user_content_key=-wMwMsjKPK7BJTS8n0Nz7gYsMPo-GoNt9BfT0y6z74OxV2QdFmLkBVzca-qbnGpPdUOh8Rz8_lVGI4u_i3kwM2joOYsczjyUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnGyHnlzZxFozotJsdfVuI4IaP-clb2BOlXhHBUak6DbflgJXRBHfWUEW7hwXYgni91Mg831PqqoYP-fm8inByanU_2KHlqvWWtz9Jw9Md8uu&lib=MoPXG4wonAChj-B1OziooWc2_kmfSSr-t'
-
-response = requests.get(url)
-goods = response.json()
-
-goods_total_cost = 0
-gluten_free_goods_cost = 0
-
-for item in goods['goods']:
-    goods_total_cost += float(item['priсe'])
-    if item['gluten_y/n']:
-        gluten_free_goods_cost += float(item['priсe'])
-
-print("Вартість всіх товарів: ", goods_total_cost)
-print("Вартість товарів без глютену: ", gluten_free_goods_cost)
+let's take that number always > 100 and only three digits
+100 <= num <= 999
+"""
 
 
+def numberToWords(num):
+    # Define the words for numbers up to 19
+    less_than_20 = [
+        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"
+    ]
+    # Define the words for tens
+    tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    # Define the words for thousands
+    thousands = ["", "Thousand", "Million", "Billion"]
+
+    if num == 0:
+        return "Zero"
+
+    def convert_three_digits(n):
+        if n == 0:
+            return ""
+        elif n < 20:
+            return less_than_20[n] + " "
+        elif n < 100:
+            return tens[n // 10] + " " + convert_three_digits(n % 10)
+        else:
+            return less_than_20[n // 100] + " Hundred " + convert_three_digits(n % 100)
+
+    result = ""
+    for i in range(len(thousands)):
+        if num % 1000 != 0:
+            result = convert_three_digits(num % 1000) + thousands[i] + " " + result
+        num //= 1000
+
+    return result.strip()
 
 
+# Test the function
+num = 333
+print(numberToWords(num))
